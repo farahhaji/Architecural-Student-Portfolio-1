54d5889f720a9b930795b9b2102e202e58597431
Farah Haji <farah@Farahs-MacBook-Pro.local>
2025-01-18 20:35:18 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
-    bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
+    name: "FARAH HAJI",
+    bio: "An aspiring designer studying at the University of Calgary, passionate about exploring the intersection of discovery, wellbeing, and belonging to cultivate meaningful experiences.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
-        linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        email: "farah.haji@ucalgary.ca",
+        phone: "(306) 716-6997",
+        linkedin: "",
+        github: "https://github.com/farahhaji",
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -16,6 +16,16 @@ const trees = [
   "./treeDrawings/tree4.jpg",
 ];
 
+const houses = [
+  "./houseDrawings/house1.png",
+  "./houseDrawings/house2.png",
+  "./houseDrawings/house3.png",
+  "./houseDrawings/house4.png",
+  "./houseDrawings/house5.png",
+  "./houseDrawings/house6.png",
+  "./houseDrawings/house8.png",
+];
+
 document.querySelector("#app").innerHTML = `
   <main id="container">
     <section id="heading">
@@ -25,24 +35,45 @@ document.querySelector("#app").innerHTML = `
 
     <section id="projects">
       <div id="project-row">
-      Model 1
+      A1: Cadavre Exquis
         <div class="three-model">
-          <div id="model1"></div>
+          <a href="./houseDrawings/house0.png" target="_blank"> 
+          <img src="./houseDrawings/house0.png" id="model1" alt="Model 1 Image">
+          </a>
+
         </div>
         <div id="images-description">
           <div id="images">
-            ${cubes
-              .map(
-                (cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`
-              )
-              .join("")}
+  <a href="./houseDrawings/house1.png" target="_blank">
+    <img src="./houseDrawings/house1.png" alt="house1" />
+  </a>
+  <a href="./houseDrawings/house2.png" target="_blank">
+    <img src="./houseDrawings/house2.png" alt="house2" />
+  </a>
+  <a href="./houseDrawings/house3.png" target="_blank">
+    <img src="./houseDrawings/house3.png" alt="house3" />
+  </a>
+  <a href="/houseDrawings/house4.png" target="_blank">
+    <img src="./houseDrawings/house4.png" alt="house4" />
+  </a>
+  <a href="houseDrawings/house5.png" target="_blank">
+    <img src="./houseDrawings/house5.png" alt="house5" />
+  </a>
+  <a href="houseDrawings/house6.png" target="_blank">
+    <img src="./houseDrawings/house6.png" alt="house6" />
+  </a>
+  <a href="houseDrawings/house8.png" target="_blank">
+    <img src="./houseDrawings/house8.png" alt="house8" />
+  </a>
+</div>
           </div>
-          <h4 id="description">A cubic 3D model is a geometric representation of a cube, consisting of six equal square faces joined at right angles. Each edge of the cube is of equal length, forming a symmetrical, rigid structure. The model can be textured, colored, or transparent, and it may include variations such as beveled edges or hollow interiors, depending on its intended purpose in design, animation, or engineering.</h4>
+          <h4 id="description"> This visual exploration delves into the application of knowledge in design and its power in shaping urban contexts, for better or worse. Design can foster agency, curiosity, and belonging, yet can also be disruptive, diminishing, and harmful. Through sketching, iterating, and returning to intention, I reflected on themes of equity and sustainability, and drew on both local and global examples. This process deepend my understanding of how knowledge application in design shapes our world, and highlighted a vital need for wisdom and care in our steps forward.</h4>
         </div>
       </div>
-
+      
+      ${/* Model 2 commented out
       <div id="project-row">
-      Model 2
+      A2: Interoperability
         <div class="three-model">
           <div id="model2"></div>
         </div>
@@ -58,31 +89,31 @@ document.querySelector("#app").innerHTML = `
           <h4 id="description">A 3D tree model is a geometric representation of a tree, featuring a trunk, branches, and foliage. The trunk is typically cylindrical, with textured bark, while the branches extend outward to support leaves or a canopy. It may include roots or flowers for added detail.</h4>
         </div>
       </div>
-
+      */ ''}
+      ${/* Model 3 commented out
       <div id="project-row">
-      Model 3
+      A3: Object Design
         <div class="three-model">
           <div id="model3"></div>
         </div>
         <div id="images-description">
           <div id="images">
-            ${cubes
+            ${houses
               .map(
-                (cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`
+                (house, index) => `<img src="${house}" alt="house${index + 1}" />`
               )
               .join("")}
           </div>
           <h4 id="description">A 3D house model is a geometric representation of a house, featuring walls, a roof, and windows. It often includes additional details such as doors, chimneys, and balconies, with textures like brick, wood, or stucco to enhance realism. The interior may also feature rooms, furniture, and lighting for a more detailed design.</h4>
         </div>
       </div>
+      */ ''}
     </section>
 
     <ul id="footer-items">
       <li>Email: ${data.contact.email}</li>
       <li>Phone: ${data.contact.phone}</li>
-      <li>LinkedIn: <a href="${data.contact.linkedin}">${
-  data.contact.linkedin
-}</a></li>
+      <li style="display:none">LinkedIn: <a href="${data.contact.linkedin}">${data.contact.linkedin}</a></li>
       <li>GitHub: <a href="${data.contact.github}">${
   data.contact.github
 }</a></li>
@@ -92,5 +123,5 @@ document.querySelector("#app").innerHTML = `
 
 // Create three.js scenes for each
 createThreeScene("#model1", "/3DModels/project1/cube.obj");
-createThreeScene("#model2", "/3DModels/project2/tree.obj");
-createThreeScene("#model3", "/3DModels/project3/cottage.obj");
+//createThreeScene("#model2", "/3DModels/project2/tree.obj");
+//createThreeScene("#model3", "/3DModels/project3/cottage.obj");
