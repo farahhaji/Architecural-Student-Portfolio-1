b95d61b3cba9d2abea2b8fb99a1bb7c077db4e9a
farahhaji <farah.haji@ucalgary.com>
2025-02-01 21:44:10 -0700

    added draft of assignment 2

--- src/main.js
+++ src/main.js
@@ -31,45 +31,87 @@ document.querySelector("#app").innerHTML = `
     <section id="heading">
       <h1>${data.name}</h1>
       <p>${data.bio}</p>
-      <p id="lol">01: CADAVRE EXQUIS / building wisdom </p>
     </section>
     <section id="projects">
+      <p id="lol">01: CADAVRE EXQUIS / building wisdom </p>
       <div id="project-row">
         <div class="three-model">
           <a href="./houseDrawings/house0.png" target="_blank"> 
-          <img src="./houseDrawings/house0.png" id="model1" alt="Model 1 Image">
+          <img src="./houseDrawings/house0.png" id="house" alt="House Image">
           </a>
-
         </div>
         <div id="images-description">
           <div id="images">
-  <a href="./houseDrawings/house1.png" target="_blank">
-    <img src="./houseDrawings/house1.png" alt="house1" />
-  </a>
-  <a href="./houseDrawings/house2.png" target="_blank">
-    <img src="./houseDrawings/house2.png" alt="house2" />
-  </a>
-  <a href="./houseDrawings/house3.png" target="_blank">
-    <img src="./houseDrawings/house3.png" alt="house3" />
-  </a>
-  <a href="/houseDrawings/house4.png" target="_blank">
-    <img src="./houseDrawings/house4.png" alt="house4" />
-  </a>
-  <a href="houseDrawings/house5.png" target="_blank">
-    <img src="./houseDrawings/house5.png" alt="house5" />
-  </a>
-  <a href="houseDrawings/house6.png" target="_blank">
-    <img src="./houseDrawings/house6.png" alt="house6" />
-  </a>
-  <a href="houseDrawings/house8.png" target="_blank">
-    <img src="./houseDrawings/house8.png" alt="house8" />
-  </a>
-</div>
+            <a href="./houseDrawings/house1.png" target="_blank">
+              <img src="./houseDrawings/house1.png" alt="house1" />
+            </a>
+            <a href="./houseDrawings/house2.png" target="_blank">
+              <img src="./houseDrawings/house2.png" alt="house2" />
+            </a>
+            <a href="./houseDrawings/house3.png" target="_blank">
+              <img src="./houseDrawings/house3.png" alt="house3" />
+            </a>
+            <a href="/houseDrawings/house4.png" target="_blank">
+              <img src="./houseDrawings/house4.png" alt="house4" />
+            </a>
+            <a href="houseDrawings/house5.png" target="_blank">
+              <img src="./houseDrawings/house5.png" alt="house5" />
+            </a>
+            <a href="houseDrawings/house6.png" target="_blank">
+              <img src="./houseDrawings/house6.png" alt="house6" />
+            </a>
+            <a href="houseDrawings/house8.png" target="_blank">
+              <img src="./houseDrawings/house8.png" alt="house8" />
+            </a>
           </div>
         </div>
-        <h4 id="description"> This visual exploration delves into the application of knowledge in design and its power in shaping urban contexts, for better or worse. Design can foster agency, curiosity, and belonging, yet can also be disruptive, diminishing, and harmful. Through sketching, iterating, and returning to intention, I reflected on themes of equity and sustainability, and drew on both local and global examples. This process deepened my understanding of how knowledge application in design shapes our world, and highlighted a vital need for wisdom and care in our steps forward.</h4>
       </div>
-      
+      <h4 id="description"> This visual exploration delves into the application of knowledge in design and its power in shaping urban contexts, for better or worse. Design can foster agency, curiosity, and belonging, yet can also be disruptive, diminishing, and harmful. Through sketching, iterating, and returning to intention, I reflected on themes of equity and sustainability, and drew on both local and global examples. This process deepened my understanding of how knowledge application in design shapes our world, and highlighted a vital need for wisdom and care in our steps forward.</h4>
+    </div>
+
+
+
+
+
+    <p id="lol">02: INTEROP(ERABILITY) / discovery </p>
+    <p> </p>
+    <div id="project-row">
+      <div class="three-model">
+         <div id="model1"></div>
+      </div>
+      <div class="three-model">
+         <div id="model2"></div>
+      </div>
+      <div class="three-model">
+         <div id="model3"></div>
+      </div>
+    </div>    
+    <div id="project-row">
+      <div class="three-model">
+         <div id="model4"></div>
+      </div>
+      <div id="images-description">
+          <div id="images">
+            <a href="./a2images/torus.png" target="_blank">
+              <img src="./a2images/torus.png" alt="torus" />
+            </a>
+          </div>
+        </div>
+      </div> 
+      <h4 id="description"> This visual exploration delves into the application of knowledge in design and its power in shaping urban contexts, for better or worse. Design can foster agency, curiosity, and belonging, yet can also be disruptive, diminishing, and harmful. Through sketching, iterating, and returning to intention, I reflected on themes of equity and sustainability, and drew on both local and global examples. This process deepened my understanding of how knowledge application in design shapes our world, and highlighted a vital need for wisdom and care in our steps forward.</h4>
+    </div>
+
+
+    
+
+
+    </section>
+
+
+
+
+
+    
       ${/* Model 2 commented out
       <div id="project-row">
       A2: Interoperability
@@ -121,6 +163,7 @@ document.querySelector("#app").innerHTML = `
 `;
 
 // Create three.js scenes for each
-createThreeScene("#model1", "/3DModels/project1/cube.obj");
-//createThreeScene("#model2", "/3DModels/project2/tree.obj");
-//createThreeScene("#model3", "/3DModels/project3/cottage.obj");
+createThreeScene("#model1", "/3DModels/project2/pavilion.obj");
+createThreeScene("#model2", "/3DModels/project2/torus.obj");
+createThreeScene("#model3", "/3DModels/project2/low_poly.obj");
+createThreeScene("#model4", "/3DModels/project2/character.obj");
